6be9763bea6456fb9a7759c78c45cc8b47afc707
Eva Lieu <itsevalieu@gmail.com>
2017-07-01 12:35:01 -0700

    Add listener

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
+// Include Server Dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mysql = require("mysql");
+
+// Create Instance of Express
+const app = express();
+
+// Sets an initial port. We'll use this later in our listener
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("./assets"));
+
+// -------------------------------------------------
+
+//	MySQL Configuration
+
+
+
+
+//Listener
+//============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
