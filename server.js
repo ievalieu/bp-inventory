649bcf9914aa4cf3539274bffed1aa860c9327d5
Eva Lieu <itsevalieu@gmail.com>
2017-07-02 23:13:55 -0700

    Connecting to mysql db

--- server.js
+++ server.js
@@ -20,8 +20,27 @@ app.use(express.static("./assets"));
 // -------------------------------------------------
 
 //	MySQL Configuration
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
 
+  // Your username
+  user: "root",
 
+  // Your password
+  password: "",
+  database: "bp-db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+// connection.query("SELECT * FROM products", function(err, res) {
+//   if (err) throw err;
+//   console.log(res);
+// });
 
 
 //Listener
